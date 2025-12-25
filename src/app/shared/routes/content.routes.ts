12fe7a71f50f44683fa7e0ca528065e0bf300074
Robin1-afk <robincastellanosperez@gmail.com>
2025-12-25 16:31:24 -0500

    first commit

---
+++ src/app/shared/routes/content.routes.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { uielementsRoutingModule } from '../../components/uielements/uielements.routes';
+import { appsRoutingModule } from '../../components/apps/apps.routes';
+import { widgetsRoutingModule } from '../../components/widgets/widgets.routes';
+import { dashboardRoutingModule } from '../../components/dashboards/dashboards.routes';
+import { pagesRoutingModule } from '../../components/pages/pages.route';
+import { mapRoutingModule } from '../../components/map/map.route';
+import { iconsRoutingModule } from '../../components/icons/icons.route';
+import { chartsRoutingModule } from '../../components/charts/charts.route';
+import { tablesRoutingModule } from '../../components/tables/tables.route';
+import { formsRoutingModule } from '../../components/forms/forms.route';
+import { advanceduiRoutingModule } from '../../components/advancedui/advancedui.routes';
+import { utilitiesRoutingModule } from '../../components/utilities/utilities.route';
+
+
+export const content: Routes = [
+
+  { path: '', children: [
+   ...dashboardRoutingModule.routes,
+   ...advanceduiRoutingModule.routes,
+   ...pagesRoutingModule.routes,
+   ...chartsRoutingModule.routes,
+   ...uielementsRoutingModule.routes,
+   ...utilitiesRoutingModule.routes,
+   ...appsRoutingModule.routes,
+   ...formsRoutingModule.routes,
+   ...mapRoutingModule.routes,
+   ...iconsRoutingModule.routes,
+   ...widgetsRoutingModule.routes,
+   ...tablesRoutingModule.routes
+   
+  ]}
+];
+@NgModule({
+    // imports: [RouterModule.forRoot()],
+    exports: [RouterModule]
+})
+export class SaredRoutingModule { }
